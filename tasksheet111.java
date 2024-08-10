261a4462f90cefd04e31fd77c1dfaa88043dd3ef
numberCruncher58 <cabsredux@gmail.com>
2024-08-10 18:32:20 +0800

    a code JVM understands

--- tasksheet111.java
+++ tasksheet111.java
@@ -1,9 +1,10 @@
 /** 
- * The HelloWorldApp class implements an application that
- * simply prints "Hello World!" to standard output.
+ * 'K A M U S T A    M U N D O'
+ * Activity 1 
  */
+ 
 public class tasksheet111 {
     public static void main(String[] args) {
-        System.out.println("Kamusta Mundo!); // Display the string.
+        System.out.println("Kamusta Mundo!");                           // Display the string 'Kamusta Mundo!'
     }
-}
+};
