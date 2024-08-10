a27a2e4790c524c85f056d42697fb02412bddf0e
numberCruncher58 <cabsredux@gmail.com>
2024-08-10 18:16:16 +0800

    Update tasksheet111.java

--- tasksheet111.java
+++ tasksheet111.java
@@ -2,7 +2,7 @@
  * The HelloWorldApp class implements an application that
  * simply prints "Hello World!" to standard output.
  */
-class helloWorldApp {
+public class tasksheet111 {
     public static void main(String[] args) {
         System.out.println("Kamusta Mundo!); // Display the string.
     }
