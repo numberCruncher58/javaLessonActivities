e1d52a3a06f6f9a3589ae928a1ab94b15cf6de57
numberCruncher58 <cabsredux@gmail.com>
2024-08-16 03:17:13 +0800

    Add files via upload

---
+++ tasksheet113.java
@@ -0,0 +1,26 @@
+/* 
+
+	T A S K   S H E E T  1 - 1 - 3
+	16 / 08 / 24
+
+*/
+
+class tasksheet113{
+	
+	public static void main(String[] args){
+		final char    beegH      = 'H' ;
+		final char    smolW	     = 'w' ;
+		final char    smolR	     = 'r' ;
+		final char    smold	     = 'd' ;
+		final float   namba2     = 2   ;
+		final byte    namba0     = 0   ;
+		final byte    namba1     = 1   ;
+		final short   numba3110  = 3110;
+		final boolean totoo      = true;	
+		String helloWorld = beegH + "" + numba3110 + " " + smolW + namba0 + smolR + namba1 + smold + " " + namba2 + " " + totoo;
+		
+		System.out.print(helloWorld);
+		
+	}
+	
+};
